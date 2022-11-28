e5054fd90981a8c12cb0564642a78601dda1c276
tearayxxz <78611540+tearayxxz@users.noreply.github.com>
2022-11-28 22:38:46 +0700

    Add files via upload

---
+++ login/js/login.js
@@ -0,0 +1,10 @@
+window.onload = pageLoad;
+
+function pageLoad(){
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	if (urlParams.get("error")==1){
+		document.getElementById('errordisplay').style.color = "red";
+		document.getElementById('errordisplay').innerHTML = "Username or password does not match.";
+	}	
+}
